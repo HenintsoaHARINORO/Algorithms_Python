94bb3a39f33a17bf06cd1d40bebaa2346ed54ea5
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-03-31 13:17:03 +0300

    factorial using recursion

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 'synatx error'
+    else:
+        return n * factorial(n - 1)
+
+
+if __name__ == '__main__':
+    n = factorial(-1)
+    print(n)
