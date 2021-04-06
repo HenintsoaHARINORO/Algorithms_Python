38e5ed33b66fb3c506be82bf9f3e423ec8c738ea
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-04-06 17:07:55 +0300

    Queue

--- Queue.py
+++ Queue.py
@@ -8,3 +8,32 @@ class Queue(object):
     def isEmpty(self):
         return self.size <=0
     def enQueue(self,item):
+        if self.size >=self.limit:
+            print('Queue overflow')
+            return 0
+        else:
+            self.que.append(item)
+        if self.front is None
+            self.front= self.rear=0
+        else:
+            self.rear= self.size
+        self.size +=1
+        print('Queue after enQueue',self.que)
+
+    def deQueue(self):
+        if self.size<=0
+            print('Queue underflow')
+            return 0
+        else:
+            self.que.pop(0)
+            self.size -=1
+            if self.size==0
+                self.front=self.rear=None
+            else:
+                self.rear=self.size -1
+                print('Queue after deQueue', self.que)
+    def queueRear(self):
+        if self.rear is None
+            print("The queue is empty")
+            raise IndexError
+        return  self.que[self.rear]
