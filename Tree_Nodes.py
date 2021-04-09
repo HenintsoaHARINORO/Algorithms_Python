5b119aa35b6d9fa72f69b6c4784ec5c1d42e61d8
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-04-09 16:59:57 +0300

    setter and getter

--- Tree_Nodes.py
+++ Tree_Nodes.py
@@ -19,3 +19,15 @@ class BinaryTree(object):
             t = BinaryTree(newNode)
             t.rightChild = self.rightChild
             self.rightChild = t
+
+    def getRightChild(self):
+        return self.rightChild
+
+    def getLeftChild(self):
+        return self.leftChild
+
+    def setRootVal(self, obj):
+        self.key = obj
+
+    def getRootVal(self):
+        return self.key
