b76f9e786e72641ac92f7f0d224eb7a1b40d0205
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-04-13 16:13:58 +0300

    arrays

--- Array_List.py
+++ Array_List.py
@@ -13,8 +13,22 @@ numbers = [10, 200, 30, 40, 50]
 print(numbers[0:3])
 print(numbers[:-2])  # print all items except the last two
 
+# O(n) time complexity
 maximum = numbers[0]
 for num in numbers:
     if num > maximum:
         maximum = num
 print(maximum)
+
+#Hacker rank
+for i in range(5):
+    if i < 5 and i >= 0:
+        print(i * i)
+
+for i in range(4):
+    if i != 0:
+        print(i, end="")
+
+firstname = "Henintsoa"
+last = "Randria"
+print("Hello " + firstname + " " + last)
