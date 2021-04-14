98842300ed3a94d739080f001f3aa28471462bf6
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-04-14 10:13:33 +0300

    Dynamic array

--- DynamicArray_Implementation.py
+++ DynamicArray_Implementation.py
@@ -1,6 +1,4 @@
 import ctypes
-
-
 class DynamicArray(object):
     def __init__(self):
         self.n = 0
@@ -21,7 +19,29 @@ class DynamicArray(object):
 
         self.A[self.n] = element
         self.n += 1
-    def _resize(self,new_cap):
-        B= self.make_array(new_cap)
+
+    def _resize(self, new_cap):
+        B = self.make_array(new_cap)
         for k in range(self.n):
-            B[k]= self.A[k]
+            B[k] = self.A[k]
+        self.A = B
+        self.capacity = new_cap
+
+    def make_array(self, new_cap):
+        return (new_cap * ctypes.py_object)()
+
+
+if __name__ == '__main__':
+    arr = DynamicArray()
+    arr.append(1)
+    arr.append(2)
+
+    arr._resize(5)
+    arr.append(3)
+    arr.append(12)
+    arr.append(5)
+    arr.append("Hello")
+    for i in range(arr.__len__()):
+        print(arr[i])
+
+
