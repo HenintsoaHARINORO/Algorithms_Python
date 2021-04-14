43c45b379bd202d631641ee76f79ff3c35ed7854
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-04-14 11:28:15 +0300

    anagram

--- Anagram.py
+++ Anagram.py
@@ -1,4 +1,11 @@
 def anagram(s1, s2):
     s1 = s1.replace(' ', '').lower()  # replace a white space
     s2 = s2.replace(' ', '').lower()
-    return sorted(s1) == sorted(s2)  # compare the two strings
+    if sorted(s1) == sorted(s2):  # compare the two strings
+        print(True)
+    else:
+        print(False)
+
+
+if __name__ == '__main__':
+    anagram('dog', 'God ')
---
+++ Anagram2.py
@@ -0,0 +1,28 @@
+def anagram2(s1, s2):
+    s1 = s1.replace(' ', '').lower()
+    s2 = s2.replace(' ', '').lower()
+
+    if len(s1) != len(s2):
+        return False
+    count = {}  # dictionary
+
+    for letter in s1:
+        if letter in count:
+            count[letter] += 1
+        else:
+            count[letter] = 1
+
+    for letter in s2:
+        if letter in count:
+            count[letter] -= 1
+        else:
+            count[letter] = 1
+
+    for k in count:
+        if count[k] != 0:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    anagram2('dog', 'God is good')
