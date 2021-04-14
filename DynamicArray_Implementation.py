685a8b0ddd90ddab34558aa794cb9d61b7047db4
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-04-14 09:50:46 +0300

    Arrays

---
+++ DynamicArray_Implementation.py
@@ -0,0 +1,27 @@
+import ctypes
+
+
+class DynamicArray(object):
+    def __init__(self):
+        self.n = 0
+        self.capacity = 1
+        self.A = self.make_array(self.capacity)
+
+    def __len__(self):
+        return self.n
+
+    def __getitem__(self, k):
+        if not 0 <= k < self.n:
+            return IndexError('K is out of bounds')
+        return self.A[k]
+
+    def append(self, element):
+        if self.n == self.capacity:
+            self._resize(2 * self.capacity)  # 2x if capacity is not enough
+
+        self.A[self.n] = element
+        self.n += 1
+    def _resize(self,new_cap):
+        B= self.make_array(new_cap)
+        for k in range(self.n):
+            B[k]= self.A[k]
