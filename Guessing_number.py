ea2a4ef0203409a5645fa0df2247190ed384fd89
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-04-15 12:06:50 +0300

    Guessing number

---
+++ Guessing_number.py
@@ -0,0 +1,44 @@
+import random
+import math
+
+lower = int(input("Enter Lower bound:- "))
+
+upper = int(input("Enter Upper bound:- "))
+
+# generating random number between
+# the lower and upper
+x = random.randint(lower, upper)
+GuessingNumber = round(math.log(upper - lower + 1, 2))
+print("\n\tYou've only ",
+      GuessingNumber,
+      " chances to guess the integer!\n")
+
+# Initializing the number of guesses.
+count = 0
+
+# for calculation of minimum number of
+# guesses depends upon range
+while count < GuessingNumber:
+    count += 1
+
+    # taking guessing number as input
+    guess = int(input("Guess a number:- "))
+
+    # Condition testing
+    if x == guess:
+        print("Congratulations you did it in ",
+              count, " try")
+        # Once guessed, loop will break
+        break
+    elif x > guess:
+        print("You guessed too small!")
+    elif x < guess:
+        print("You Guessed too high!")
+
+# If Guessing is more than required guesses,
+# shows this output.
+if count >=GuessingNumber:
+    print("\nThe number is %d" % x)
+    print("\tBetter Luck Next time!")
+
+
