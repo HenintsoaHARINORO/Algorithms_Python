29290d32ca929c88768674591fcf430d687a1e3e
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-04-15 11:37:01 +0300

    binary search fixed

--- Binary_Search.py
+++ Binary_Search.py
@@ -2,17 +2,17 @@ def binary_search_Index(list, item):
     firstIndex = 0
     lastIndex = len(list) - 1
     while firstIndex <= lastIndex:
-        lastElement = firstIndex + lastIndex
-        guess = list[lastElement]
+        middle = (firstIndex + lastIndex) // 2
+        guess = list[middle]
         if guess == item:
-            return lastElement
+            return middle
         if guess > item:
-            lastIndex = lastElement - 1 # go back to while loop
+            lastIndex = middle - 1  # go back to while loop
         else:
-            firstIndex = lastElement + 1
+            firstIndex = middle + 1
     return None
 
 
 if __name__ == '__main__':
-    list = [1, 3, 5, 7, 9]
+    list = [1, 3, 5, 7, 9, 12, 34]
     print(binary_search_Index(list, 3))
