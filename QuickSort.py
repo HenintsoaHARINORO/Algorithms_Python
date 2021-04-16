84d7824540f4ade481765b8d62f6ae2aea025ea4
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-04-16 14:13:49 +0300

    quick sort

---
+++ QuickSort.py
@@ -0,0 +1,12 @@
+def quickSort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+        greater = [i for i in arr[1:] if i > pivot]
+        return quickSort(less) + [pivot] + quickSort(greater)
+
+
+if __name__ == '__main__':
+    print(quickSort([2, 5, 8, 3, -3]))
