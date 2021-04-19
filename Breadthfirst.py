44b31875b1d743e2363cf5a4c746bdd4172edd56
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-04-19 14:40:44 +0300

    breadth first search

---
+++ Breadthfirst.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+
+def person_is_seller(name):
+    return name[-1] == "m"
+
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+                print(person + " is a mango seller")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    return False
+
+
+if __name__ == '__main__':
+    graph = {}
+    graph["me"] = ["alice", "bobm", "claire"]
+    graph["bob"] = ["anuj", "peggy"]
+    graph["alice"] = ["peggy"]
+    graph["claire"] = ["thom", "jonny"]
+    graph["anuj"] = []
+    graph["peggy"] = []
+    graph["thom"] = []
+    graph["jonny"] = []
+    print(search("me"))
