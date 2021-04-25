909f8688ab3482e5025fb5f9b53aeb667dc7f6d3
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-04-25 16:49:42 +0300

    Doubly Linked List

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -3,3 +3,14 @@ class DoublyLinkedListNode(object):
         self.value = value
         self.previousNode = None
         self.nextNode = None
+
+
+if __name__ == "__main__":
+    a = DoublyLinkedListNode(1)
+    b = DoublyLinkedListNode(2)
+    c = DoublyLinkedListNode(3)
+
+    b.previousNode = a
+    a.nextNode = b
+    b.nextNode = c
+    c.previousNode = b
