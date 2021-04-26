28842108e26c3685563ce6952d64713eea0a7f72
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-04-26 14:50:57 +0300

    reformated code

--- LinkedListCycleCheck.py
+++ LinkedListCycleCheck.py
@@ -1,29 +1,30 @@
 class Node(object):
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
-        self .nextNode =  None
+        self.nextNode = None
+
+
 def cycle_check(node):
     marker1 = node
     marker2 = node
     while marker2 != None and marker2.nextNode != None:
         marker1 = marker1.nextNode
         print(marker1.value)
-        marker2 = marker2 .nextNode.nextNode
+        marker2 = marker2.nextNode.nextNode
 
         print(marker2.value)
         if marker2 == marker1:
             return True
     return False
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     a = Node(1)
     b = Node(2)
     c = Node(3)
 
     a.nextNode = b
-    b .nextNode = c
-    c .nextNode = a # cycle
-
+    b.nextNode = c
+    c.nextNode = a  # cycle
 
     print(cycle_check(c))
-
