dad52e3fe94e6bc4bcddf3504f4546903fe8642b
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-04-26 14:18:47 +0300

    nth linked list

--- LinkedListNth.py
+++ LinkedListNth.py
@@ -6,18 +6,19 @@ class Node:
         self.value = value
         self.nextNode = None
 
-    def nth_to_last_node(n, head):
-        left_pointer = head
-        right_pointer = head
 
-        for i in xrange(n - 1):
-            if not right_pointer.nextNode:
-                raise LookupError('Error: n is larger than the linked list.')
-            right_pointer = right_pointer.nextNode
-        while right_pointer.nextNode:
-            left_pointer = left_pointer.nextNode
-            right_pointer = right_pointer.nextNode
-        return left_pointer
+def nth_to_last_node(n, head):
+    left_pointer = head
+    right_pointer = head
+
+    for i in xrange(n - 1):
+        if not right_pointer.nextNode:
+            raise LookupError('Error: n is larger than the linked list.')
+        right_pointer = right_pointer.nextNode
+    while right_pointer.nextNode:
+        left_pointer = left_pointer.nextNode
+        right_pointer = right_pointer.nextNode
+    return left_pointer
 
 
 if __name__ == "__main__":
@@ -31,3 +32,5 @@ if __name__ == "__main__":
     b.nextNode = c
     c.nextNode = d
     d.nextNode = e
+
+    print(nth_to_last_node(1,a).value)
