53f88949958dfc18e6be380c87db855a0637f1d5
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-04-28 09:39:11 +0300

    Word split

---
+++ wordSplit.py
@@ -0,0 +1,10 @@
+def word_split(phrase,list_of_words,output = None):
+    if output is None:
+        output = []
+    for word in list_of_words:
+        if phrase.startswith(word):
+           output.append(word)
+           return word_split(phrase[len(word):], list_of_words, output)
+
+    return output
+print(word_split('Iloveyou',['I','am','in','love','with','you']))
