2d70202e24394ad976c0e4639495eb56c3d6abc4
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-04-29 14:07:53 +0300

    String permutation using recursion

---
+++ RecursionStringPermutation.py
@@ -0,0 +1,11 @@
+def permute(s):
+    output = []
+
+    if len(s)<=1:
+        output = [s]
+    else:
+        for i , let in enumerate(s):
+            for perm in permute(s[:i]+s[i+1:]):
+                output +=[let+perm]
+    return output
+print(permute("abc"))
