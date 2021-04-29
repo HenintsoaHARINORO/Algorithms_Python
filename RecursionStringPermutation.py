15379136132459ac04272024f98bd529c9561846
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-04-29 14:16:19 +0300

    last changes

--- RecursionStringPermutation.py
+++ RecursionStringPermutation.py
@@ -1,11 +1,13 @@
 def permute(s):
     output = []
 
-    if len(s)==1:
+    if len(s) == 1:
         output = [s]
     else:
-        for i , let in enumerate(s):
-            for perm in permute(s[:i]+s[i+1:]):
-                output +=[let+perm]
+        for i, let in enumerate(s):
+            for perm in permute(s[:i] + s[i + 1:]):
+                output += [let + perm]
     return output
+
+
 print(permute("abc"))
