2857bb8571d1d3d4de41ea165ec004fc7ae0901e
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-01 10:33:35 +0300

    print tree level order

--- TreeLevelOrderPrint.py
+++ TreeLevelOrderPrint.py
@@ -1,5 +1,25 @@
+from collections import deque
+
+
 class Node:
     def __init__(self, value=None):
         self.left = None
         self.right = None
         self.value = value
+
+
+def levelOrderPrint(tree):
+    if tree is None:
+        return None
+    queue = deque()  # why deque?because it has two ends
+    queue.append(tree)
+    while len(queue) != 0:
+        temp = deque()
+        while len(queue) != 0:
+            node = queue.pop()
+            print(str(node.value) + ' ')
+            if node.left is not None:
+                temp.append(tree.left)
+            if node.right is not None:
+                temp.append(tree.right)
+            queue = temp
