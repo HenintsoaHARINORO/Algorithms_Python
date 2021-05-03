d68f609772c5833e2067c01fc84d5837e36ac811
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-03 08:54:38 +0300

    Graphs Implementation

--- Graphs.py
+++ Graphs.py
@@ -16,3 +16,20 @@ class Node:
 
     def __str__(self):
         return str(self.num)
+
+
+class Graph:
+    def __init__(self):
+        self.nodes = OrderedDict()
+
+    def add_node(self, num):
+        node = Node(num)
+        self.nodes[num] = node
+        return node
+
+    def add_edge(self, source, dest, weight=0):
+        if source not in self.nodes:
+            self.add_node(source)
+        if dest not in self.nodes:
+            self.add_node(dest)
+        self.nodes[source].adjacent[self.nodes[dest]] = weight
