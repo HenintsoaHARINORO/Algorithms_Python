86936ab1b7be630f46a9713ef4a55f7a58d4881d
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-03 10:27:16 +0300

    adjacency List Graph

--- GraphAdjacencyList.py
+++ GraphAdjacencyList.py
@@ -7,7 +7,7 @@ class Vertex:
         self.connectedTo[nbr] = weight
 
     def __str__(self):
-        return str(self.id) + 'connectedTo:' + str([x.id for x in self.connectedTo])
+        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
 
     def getConnections(self):
         return self.connectedTo.keys()
@@ -18,30 +18,47 @@ class Vertex:
     def getWeight(self, nbr):
         return self.connectedTo[nbr]
 
+
 class Graph:
     def __init__(self):
         self.vertList = {}
         self.numVertices = 0
-    def addVertex(self,key):
+
+    def addVertex(self, key):
         self.numVertices += 1
         newVertex = Vertex(key)
         self.vertList[key] = newVertex
         return newVertex
-    def getVertex(self,n):
+
+    def getVertex(self, n):
         if n in self.vertList:
-            return  self.vertList[n]
+            return self.vertList[n]
         else:
             return None
+
     def __contains__(self, n):
         return n in self.vertList
-    def addEdge(self,f,t,cost = 0):
+
+    def addEdge(self, f, t, cost=0):
         if f not in self.vertList:
             nv = self.addVertex(f)
         if t not in self.vertList:
             nv = self.addVertex(t)
-        self.vertList[f].addNeighbor(self.vertList[t],cost)
+        self.vertList[f].addNeighbor(self.vertList[t], cost)
+
     def getVertices(self):
         return self.vertList.keys()
+
     def __iter__(self):
         return iter(self.vertList.values())
 
+
+g = Graph()
+for i in range(6):
+    g.addVertex(i)
+g.addEdge(0, 1, 2)
+g.addEdge(1, 2)
+for vertex in g:
+    print(vertex)
+    print(vertex.getConnections())
+    print('\n')
