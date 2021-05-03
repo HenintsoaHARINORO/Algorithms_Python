18346a9144eb3ee2f9170bd456af52aec47ca77f
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-03 12:43:48 +0300

    BreadFirstSearch

--- GraphBreadFirstSearch.py
+++ GraphBreadFirstSearch.py
@@ -1,12 +1,5 @@
-
-graph = {'A': set(['B', 'C']),
-         'B': set(['A', 'D', 'E']),
-         'C': set(['A', 'F']),
-         'D': set(['B']),
-         'E': set(['B', 'F']),
-         'F': set(['C', 'E'])}
-def bfs(graph,start):
-    visited ,queue = set() ,[start]
+def bfs(graph, start):
+    visited, queue = set(), [start]
     while queue:
         vertex = queue.pop(0)
         if vertex not in visited:
@@ -14,15 +7,24 @@ def bfs(graph,start):
             queue.extend(graph[vertex] - visited)
     return visited
 
-def bfs_paths(graph,start,goal):
-    queue = [(start,[start])]
+
+def bfs_paths(graph, start, goal):
+    queue = [(start, [start])]
     while queue:
-        (vertex , path) = queue.pop(0)
+        (vertex, path) = queue.pop(0)
         for next in graph[vertex] - set(path):
             if next == goal:
                 yield path + [next]
             else:
                 queue.append((next, path + [next]))
-if __name__=='__main__':
-    print(bfs(graph,'A'))
+
+
+if __name__ == '__main__':
+    graph = {'A': set(['B', 'C']),
+             'B': set(['A', 'D', 'E']),
+             'C': set(['A', 'F']),
+             'D': set(['B']),
+             'E': set(['B', 'F']),
+             'F': set(['C', 'E'])}
+    print(bfs(graph, 'A'))
     print(list(bfs_paths(graph, 'A', 'D')))
