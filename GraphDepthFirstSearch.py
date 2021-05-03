0d765f0a65a4c1f5eb09a9b2c50ac819b0f5a723
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-03 13:39:02 +0300

    Depth First Search

--- GraphDepthFirstSearch.py
+++ GraphDepthFirstSearch.py
@@ -17,7 +17,15 @@ def dfs2(graph, start, visited=None):
         dfs(graph, nxt, visited)
     return visited
 
-
+def dfs_paths(graph,start,goal):
+    stack = [(start,[start])]
+    while stack:
+        (vertex , path) = stack.pop()
+        for nxt in graph[vertex] - set(path):
+            if nxt == goal:
+                yield path + [nxt]
+            else:
+                stack.append((nxt,path+[nxt]))
 if __name__ == '__main__':
     graph = {'A': set(['B', 'C']),
              'B': set(['A', 'D', 'E']),
@@ -27,3 +35,4 @@ if __name__ == '__main__':
              'F': set(['C', 'E'])}
 
     print(dfs(graph, 'A'))
+    print(list(dfs_paths(graph,'A','F')))
