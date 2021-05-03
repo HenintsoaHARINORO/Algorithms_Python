ddc20fed96eff47620bf1778ed1b839d890c8055
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-03 11:52:16 +0300

    Graph Bread First Search

--- GraphBreadFirstSearch.py
+++ GraphBreadFirstSearch.py
@@ -1,8 +1,28 @@
+
 graph = {'A': set(['B', 'C']),
          'B': set(['A', 'D', 'E']),
          'C': set(['A', 'F']),
          'D': set(['B']),
          'E': set(['B', 'F']),
-         'F': set(['C', 'E'])
+         'F': set(['C', 'E'])}
+def bfs(graph,start):
+    visited ,queue = set() ,[start]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+    return visited
 
-         }
+def bfs_paths(graph,start,goal):
+    queue = [(start,[start])]
+    while queue:
+        (vertex , path) = queue.pop(0)
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                queue.append((next, path + [next]))
+if __name__=='__main__':
+    print(bfs(graph,'A'))
+    print(list(bfs_paths(graph, 'A', 'D')))
