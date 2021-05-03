3fd5309f23c9616a4b079c33d9dfd0a8d98e6bd9
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-03 13:33:04 +0300

    Depth First Search

---
+++ GraphDepthFirstSearch.py
@@ -0,0 +1,29 @@
+def dfs(graph, start):
+    visited = set()
+    stack = [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+    return visited
+
+
+def dfs2(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    for nxt in graph[start] - visited:
+        dfs(graph, nxt, visited)
+    return visited
+
+
+if __name__ == '__main__':
+    graph = {'A': set(['B', 'C']),
+             'B': set(['A', 'D', 'E']),
+             'C': set(['A', 'F']),
+             'D': set(['B']),
+             'E': set(['B', 'F']),
+             'F': set(['C', 'E'])}
+
+    print(dfs(graph, 'A'))
