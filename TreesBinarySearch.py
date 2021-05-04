7ead8281e5237cf9a5201908cd3aa0da36d80a8b
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-04 16:42:45 +0300

    BST

--- TreesBinarySearch.py
+++ TreesBinarySearch.py
@@ -198,3 +198,13 @@ class BinarySearchTree:
                                                 currentNode.rightChild.payload,
                                                 currentNode.rightChild.leftChild,
                                                 currentNode.rightChild.rightChild)
+
+
+mytree = BinarySearchTree()
+mytree[3]="red"
+mytree[4]="blue"
+mytree[6]="yellow"
+mytree[2]="at"
+
+print(mytree[6])
+print(mytree[2])
