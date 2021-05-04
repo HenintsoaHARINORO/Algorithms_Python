66d377c903a5b05e01f3cab82bc9ebe5e46a55ba
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-04 16:34:09 +0300

    BST: delete a Node

--- TreesBinarySearch.py
+++ TreesBinarySearch.py
@@ -119,3 +119,38 @@ class BinarySearchTree:
 
     def __delitem__(self, key):
         self.delete(key)
+
+    def remove(self, currentNode):
+        if currentNode.isLeaf():  # leaf:has no children
+            if currentNode == currentNode.parent.leftChild:
+                currentNode.parent.leftChild = None
+            else:
+                currentNode.parent.rightChild = None
+        elif currentNode.hasBothChildren():
+            succ = currentNode.findSuccessor()
+            succ.spliceOut()
+            currentNode.key = succ.key
+            currentNode.payload = succ.payload
+        else:  # the node has one child
+            if currentNode.hasLeftChild:
+                if currentNode.isLeftChild():
+                    currentNode.leftChild.parent = currentNode.parent
+                    currentNode.parent.leftChild = currentNode.leftChild
+                elif currentNode.isRightChild():
+                    currentNode.leftChild.parent = currentNode.parent
+                    currentNode.parent.rightChild = currentNode.leftChild
+                else:
+                    currentNode.replaceNodeData(currentNode.leftChild.key, currentNode.leftChild.payload,
+                                                currentNode.leftChild.leftChild, currentNode.leftChild.rightChild)
+            else:
+                if currentNode.isLeftChild():
+                    currentNode.rightChild.parent = currentNode.parent
+                    currentNode.parent.leftChild = currentNode.rightChild
+                elif currentNode.isRightChild():
+                    currentNode.rightChild.parent = currentNode.parent
+                    currentNode.parent.rightChild = currentNode.rightChild
+                else:
+                    currentNode.replaceNodeData(currentNode.rightChild.key,
+                                                currentNode.rightChild.payload,
+                                                currentNode.rightChild.leftChild,
+                                                currentNode.rightChild.rightChild)
