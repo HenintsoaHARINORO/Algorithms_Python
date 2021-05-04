54675c23b971ec320f2ee3a090abb58b0882d6c2
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-04 13:35:57 +0300

    Tree Node

---
+++ TreesBinarySearch.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, key, value, left=None, right=None, parent=None):
+        self.key = key
+        self.payload = value
+        self.leftChild = left
+        self.rightChild = right
+        self.parent = parent
+
+    def hasLeftChild(self):
+        return self.leftChild
+
+    def hasRightChild(self):
+        return self.rightChild
+
+    def isLeftChild(self):
+        return self.parent and self.parent.leftChild == self
+
+    def isRightChild(self):
+        return self.parent and self.parent.rightChild == self
