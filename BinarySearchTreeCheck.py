762ff8c9bdead9d71c2e33a306a1d4a8a0dae0ee
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-07 16:38:31 +0300

    BST check

--- BinarySearchTreeCheck.py
+++ BinarySearchTreeCheck.py
@@ -1,19 +1,39 @@
 class Node:
-    def __init__(self,k,val):
+    def __init__(self, k, val):
         self.key = k
-        self.value= val
+        self.value = val
         self.left = None
         self.right = None
-    def tree_max(node):
-        if not node:
-            return float("-inf")
-        maxleft = tree_max(node.left)
-        maxright = tree_max(node.right)
-        return max(node.key,maxleft,maxright)
 
-    def tree_min(node):
-        if not node:
-            return float("inf")
-        minleft = tree_min(node.left)
-        minright = tree_min(node.right)
-        return min(node.key, minleft, minright)
+
+def tree_max(node):
+    if not node:
+        return float("-inf")
+    maxleft = tree_max(node.left)
+    maxright = tree_max(node.right)
+    return max(node.key, maxleft, maxright)
+
+
+def tree_min(node):
+    if not node:
+        return float("inf")
+    minleft = tree_min(node.left)
+    minright = tree_min(node.right)
+    return min(node.key, minleft, minright)
+
+
+def verify(node):
+    if not node:
+        return True
+    if (tree_max(node.left) <= node.key <= tree_min(node.right) and verify(node.left) and verify(node.right)):
+        return True
+    else:
+        return False
+
+
+root = Node(10, "Hello")
+root.left = Node(5, "Five")
+root.right = Node(30, "Thirty")
+root.left.right = Node(15, "Fifteen")
+
+print(verify(root))
