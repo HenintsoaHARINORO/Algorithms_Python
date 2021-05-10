96bd1cfe07be8d90198ef835b37403cc2de398f1
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-10 15:24:04 +0300

    Implementation

--- HashTable.py
+++ HashTable.py
@@ -6,7 +6,7 @@ class HashTable(object):
 
     def put(self, key, data):
         hashvalue = self.hashfunction(key, len(self.slots))
-        if self.slots[hashvalue] == None:
+        if self.slots[hashvalue] is None:
             self.slots[hashvalue] = key
             self.data[hashvalue] = data
         else:
@@ -15,10 +15,10 @@ class HashTable(object):
             else:
                 nextslot = self.rehash(hashvalue, len(self.slots))
 
-                while self.slots[nextslot] != None and self.slots[nextslot] != key:
+                while self.slots[nextslot] is not None and self.slots[nextslot] != key:
                     nextslot = self.rehash(nextslot, len(self.slots))
 
-                if self.slots[nextslot] == None:
+                if self.slots[nextslot] is None:  # set the new key
                     self.slots[nextslot] = key
                     self.data[nextslot] = data
 
@@ -31,3 +31,35 @@ class HashTable(object):
 
     def rehash(self, oldhash, size):
         return (oldhash + 1) % size  # find the new slot
+
+    def get(self, key):
+        startslot = self.hashfunction(key, len(self.slots))
+        data = None
+        stop = False
+        found = False
+        position = startslot
+
+        while self.slots[position] is not None and not found and not stop:
+
+            if self.slots[position] == key:
+                found = True
+                data = self.data[position]
+
+            else:  # otherwise continue going
+                position = self.rehash(position, len(self.slots))
+                if position == startslot:
+                    stop = True
+        return data
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, data):
+        self.put(key, data)
+
+h = HashTable(5)
+h[1] = 'one'
+h[2] = 'two'
+h[3] = 'three'
+print(h[1])
+print(h[2])
