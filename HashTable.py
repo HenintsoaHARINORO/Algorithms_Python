9cc2de7095e7d57ad2b4549e3c46c63a24889861
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-10 15:07:16 +0300

    Implementation a Hash Table

---
+++ HashTable.py
@@ -0,0 +1,33 @@
+class HashTable(object):
+    def __init__(self, size):
+        self.size = size
+        self.slots = [None] * self.size  # [None](a list) * 4 ->[None,None,None,None]
+        self.data = [None] * self.size
+
+    def put(self, key, data):
+        hashvalue = self.hashfunction(key, len(self.slots))
+        if self.slots[hashvalue] == None:
+            self.slots[hashvalue] = key
+            self.data[hashvalue] = data
+        else:
+            if self.slots[hashvalue] == key:
+                self.data[hashvalue] = data
+            else:
+                nextslot = self.rehash(hashvalue, len(self.slots))
+
+                while self.slots[nextslot] != None and self.slots[nextslot] != key:
+                    nextslot = self.rehash(nextslot, len(self.slots))
+
+
+                if self.slots[nextslot] == None:
+                    self.slots[nextslot] = key
+                    self.data[nextslot] = data
+
+
+                else:
+                    self.data[nextslot] = data
+
+    def hashfunction(self, key, size):
+       return  key%size
+    def rehash(self,oldhash,size):
+       return (oldhash + 1)%size # find the new slot
