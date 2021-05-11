2bcc18463b12a68ff784c42debb67230b7253d21
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-11 13:22:02 +0300

    quick Sort

--- QuickSort.py
+++ QuickSort.py
@@ -3,8 +3,10 @@ def quickSort(arr):
         return arr
     else:
         pivot = arr[0]
-        less = [i for i in arr[1:] if i <= pivot]
-        greater = [i for i in arr[1:] if i > pivot]
+        less = [i for i in arr[1:] if i <= pivot]  # elements that are less or equal than the pivot
+        print(less)
+        greater = [i for i in arr[1:] if i > pivot]  # elements that are greater than the pivot
+        print(greater)
         return quickSort(less) + [pivot] + quickSort(greater)
 
 
