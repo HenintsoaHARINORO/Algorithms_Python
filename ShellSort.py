3defdc7b1372577cf9da5563f53d11c4677f812c
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-13 09:52:50 +0300

    Shell Sort

--- ShellSort.py
+++ ShellSort.py
@@ -1,9 +1,25 @@
 def shellSort(array):
-   sublistCount = len(array) // 2
+    sublistCount = len(array) // 2
 
-   while sublistCount > 0:
-       for start in range(sublistCount):
-           gap_insertion_sort(array,start,sublistCount)
+    while sublistCount > 0:
+        for start in range(sublistCount):
+            gap_insertion_sort(array, start, sublistCount)
 
-       sublistCount = sublistCount // 2
-def gap_insertion_sort(array,start,gap):
+        sublistCount = sublistCount // 2
+
+
+def gap_insertion_sort(array, start, gap):
+    for i in range(start + gap, len(array), gap):
+        print(i)
+        currentValue = array[i]
+        position = i
+
+        while position >= gap and array[position - gap] > currentValue:
+            array[position] = array[position - gap]
+            position = position - gap
+        array[position] = currentValue
+
+
+array = [23, 13, 0, -5, 3]
+shellSort(array)
+print(array)
