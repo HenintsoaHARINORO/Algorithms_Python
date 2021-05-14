d7a1ba842b0309768476c374eccca8ed144bfa7f
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-14 16:30:54 +0300

    Word Ladder

--- GraphsWordLadder.py
+++ GraphsWordLadder.py
@@ -1,2 +1,16 @@
 class Vertex:
-    def __init__(self,key):
+    def __init__(self, key):
+        self.id = key
+        self.connectedTo = {}
+
+    def addNeighbor(self,nbr,weight = 0):
+        self.connectedTo[nbr] = weight
+
+    def __str__(self):
+        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
+
+    def getConnections(self):
+        return self.connectedTo.keys()
+
+    def getId(self):
+        return self.id
