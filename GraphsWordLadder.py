5751f3820ee8576f6bf37f17dc58e8e747574a67
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-17 16:11:41 +0300

    Graphs

--- GraphsWordLadder.py
+++ GraphsWordLadder.py
@@ -59,3 +59,12 @@ class Graph:
         wfile = open(wordFile, 'r')
         for line in wfile:
             print(line)
+            word = line[:-1]
+            print(word)
+            for i in range(len(word)):
+                bucket = word[:i] + '_' + word[i+1:]
+                if bucket in d:
+                    d[bucket].append(word)
+                else:
+                    d[bucket] = [word]
+
