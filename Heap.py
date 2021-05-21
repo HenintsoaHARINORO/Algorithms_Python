50b5aa3e0af51545f0eefc6c16ede2f708bbaf82
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-21 12:07:36 +0300

    Heap implementation

--- Heap.py
+++ Heap.py
@@ -2,3 +2,12 @@ from heapq import heappop, heappush, heapify
 
 heap = []
 nums = [12, 3, -2, 6, 4, 8, 9]
+
+# for num in nums:
+# heappush(heap,nums)
+
+# while heap:
+#  print(heappop(heap)) # return the root node on every iteration
+
+heapify(nums)
+print(nums)
