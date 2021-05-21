211c8c3c8f09de2aced6db9082e5cb93474dee4d
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-21 12:50:49 +0300

    Heap implementation

--- Heap2.py
+++ Heap2.py
@@ -2,9 +2,10 @@ class Heap(object):
     HEAP_SIZE = 10
 
     def __init__(self):
-        self.heap = [0] *Heap.HEAP_SIZE
+        self.heap = [0] * Heap.HEAP_SIZE
         self.currentPosition = -1
-    def insert(self,item):
+
+    def insert(self, item):
         if self.isFull():
             print("Heap is full")
             return
@@ -12,20 +13,16 @@ class Heap(object):
         self.heap[self.currentPosition] = item
         self.fixUp(self.currentPosition)
 
-    def fixUp(self,index):
-        parentIndex = int((index- 1)/2)
-        while parentIndex >=0 and self.heap[parentIndex] < self.heap[index]:
+    def fixUp(self, index):
+        parentIndex = int((index - 1) / 2)
+        while parentIndex >= 0 and self.heap[parentIndex] < self.heap[index]:
             temp = self.heap[index]
             self.heap[index] = self.heap[parentIndex]
-            self.heap[parentIndex] =temp
-            parentIndex= int((index-1)/2)
-
+            self.heap[parentIndex] = temp
+            parentIndex = int((index - 1) / 2)
 
     def isFull(self):
-        if self.currentPosition == Heap.HEAP_SIZE
-            return  True
+        if self.currentPosition == Heap.HEAP_SIZE:
+            return True
         else:
             return False
-
-
-
