32bb8ce29e24dfe0d6e6769826f00c9688371197
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-22 14:02:37 +0300

    Heap

--- Heap2.py
+++ Heap2.py
@@ -21,6 +21,14 @@ class Heap(object):
             self.heap[parentIndex] = temp
             parentIndex = int((index - 1) / 2)
 
+    def heapsort(self):
+        for i in range(0,self.currentPosition):
+            temp = self.heap[0]
+            print("%d" %temp)
+            self.heap[0] = self.heap[self.currentPosition - i]
+            self.heap[self.currentPosition - i] = temp
+            self.fixDown(0,self.currentPosition - i - 1)
+
     def isFull(self):
         if self.currentPosition == Heap.HEAP_SIZE:
             return True
