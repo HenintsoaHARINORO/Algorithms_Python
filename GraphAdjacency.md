7580e8bf3e95ca43758d20c67b067343d2ad5ca6
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-05-31 14:31:36 +0300

    Graphs Adjacency List Implementation

--- GraphAdjacency.md
+++ GraphAdjacency.md
@@ -1,2 +1,4 @@
 ##Adjacency Matrix:
 Implement a graph using a two-dimensional matrix.
+* It is a good implementation for a graph when the number of edges is large
+## Adjacency List:
--- GraphAdjacencyList.py
+++ GraphAdjacencyList.py
@@ -1,13 +1,14 @@
 class Vertex:
     def __init__(self, key):
         self.id = key
-        self.connectedTo = {}
+        self.connectedTo = {}  # dictionary
 
-    def addNeighbor(self, nbr, weight=0):
-        self.connectedTo[nbr] = weight
+    def addNeighbor(self, neighbor, weight=0):
+        self.connectedTo[
+            neighbor] = weight  # nbr= neighbor as the key and weight as a value of the connectedTo dictionary
 
     def __str__(self):
-        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
+        return str(self.id) + ' connected to: ' + str([x.id for x in self.connectedTo])
 
     def getConnections(self):
         return self.connectedTo.keys()
@@ -15,8 +16,8 @@ class Vertex:
     def getId(self):
         return self.id
 
-    def getWeight(self, nbr):
-        return self.connectedTo[nbr]
+    def getWeight(self, neighbor):
+        return self.connectedTo[neighbor]
 
 
 class Graph:
@@ -31,7 +32,7 @@ class Graph:
         return newVertex
 
     def getVertex(self, n):
-        if n in self.vertList:
+        if n in self.vertList:  # dictionary keys
             return self.vertList[n]
         else:
             return None
@@ -39,26 +40,27 @@ class Graph:
     def __contains__(self, n):
         return n in self.vertList
 
-    def addEdge(self, f, t, cost=0):
-        if f not in self.vertList:
-            nv = self.addVertex(f)
-        if t not in self.vertList:
-            nv = self.addVertex(t)
-        self.vertList[f].addNeighbor(self.vertList[t], cost)
+    def addEdge(self, fromVertex, toVertex, cost=0):
+        if fromVertex not in self.vertList:
+            nv = self.addVertex(fromVertex)
+        if toVertex not in self.vertList:
+            nv = self.addVertex(toVertex)
+        self.vertList[fromVertex].addNeighbor(self.vertList[toVertex], cost)
 
     def getVertices(self):
         return self.vertList.keys()
 
-    def __iter__(self):
+    def __iter__(self):  # iteration
         return iter(self.vertList.values())
 
 
 g = Graph()
 for i in range(6):
     g.addVertex(i)
-g.addEdge(0, 1, 2)
-g.addEdge(1, 2)
-for vertex in g:
+print(g.vertList)
+g.addEdge(0, 1, 2)  # from Vertex 0 to Vertex 1 weight= 2
+
+for vertex in g: # take advantages of __contains__ and __iter__
     print(vertex)
     print(vertex.getConnections())
     print('\n')
